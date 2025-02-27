d5962d163de429ea32f8c96e8c6c3c63e5dd7c93
Katlyn Alo <26312638+Kat-Alo@users.noreply.github.com>
2025-02-27 10:13:53 -0800

    improving, adding comments wrangle script

--- scripts/wrangle_hmda_data.py
+++ scripts/wrangle_hmda_data.py
@@ -26,6 +26,9 @@ s3_client = boto3.client(
 )
 
 def file_exists_in_s3(file_path):
+    """
+    Accepts an S3 file key ("path", e.g. "raw-data/some-file.json") and checks if the file exists in the S3 bucket.
+    """
     try:
         s3_client.head_object(Bucket=S3_BUCKET, Key=f"{file_path}")
         return True
@@ -37,12 +40,19 @@ def file_exists_in_s3(file_path):
             raise
 
 def fetch_data(params):
+    """
+    Fetches CSV data from the HMDA API using the provided year and loan purpose parameters.
+    Note: as of writing, this HMDA endpoint only accepts a min of 1 param, maximum of 2 — despite how large these files are.
+    """
     print(f"Fetching data for {params["years"]}: loan purpose {params["loan_purposes"]}")
     response = requests.get(API_ENDPOINT, params=params)
     response.raise_for_status()
-    return response.text  # Assuming CSV response
+    return response.text
 
 def upload_to_s3(csv_data, filename, year):
+    """
+    Uploads data as CSV to S3 bucket. Path is determined by the year and loan purpose, e.g. "raw-data/2021/hmda_data_loan_purpose_1.csv"
+    """
     csv_buffer = StringIO(csv_data)
     s3_client.put_object(Bucket=S3_BUCKET, Key=f"{S3_PREFIX}/{year}/{filename}", Body=csv_buffer.getvalue())
     print(f"Uploaded {year} {filename} to S3")
@@ -58,6 +68,9 @@ for year in YEARS:
         filename = f"hmda_data_loan_purpose_{loan_purpose}.csv"
         file_path = f"{S3_PREFIX}/{year}/{filename}"
 
+        # This is a just-in-case measure because these files are so big that there's a decent chance there'll
+        # be a disruption in the middle of fetching them, and we'll have to start again. This way we can skip
+        # files that have already been fetched.
         if file_exists_in_s3(file_path):
             print(f"File {file_path} already exists in S3. Skipping...")
             continue
